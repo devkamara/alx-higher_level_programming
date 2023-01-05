5164a7ed030107290e8320fae346182d280e9526
Devkamara <chegerah@gmail.com>
2023-01-05 11:27:29 -0800

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -41,33 +41,33 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-        def area(self):
-            """Return the area of the Rectangle."""
-            return (self.__width * self.__height)
+    def area(self):
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
-        def perimeter(self):
-            """Return the perimeter of the Rectangle."""
-            if self.__width == 0 or self.__height == 0:
-                return (0)
-            return ((self.__width * 2) + (self.__height * 2))
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
-        def __str__(self):
-            """
-            returns the printable repr of the rectangle
-            """
-            if self.__width == 0 or self.__height == 0:
-                return ("")
-            r = []
-            for i in range(self.__height):
-                [r.append('#') for j in range(self.__width)]
-                if i != self.__height - 1:
-                    r.append("\n")
-            return ("".join(r))
+    def __str__(self):
+        """
+        returns the printable repr of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        r = []
+        for i in range(self.__height):
+            [r.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                r.append("\n")
+        return ("".join(r))
 
-        def __repr__(self):
-            """
-            str represantation of the Rectangle
-            """
-            r = "Rectangle(" + str(self.__width)
-            r += ", " + str(self.__height) + ")"
-            return (r)
+    def __repr__(self):
+        """
+        str represantation of the Rectangle
+        """
+        r = "Rectangle(" + str(self.__width)
+        r += ", " + str(self.__height) + ")"
+        return (r)
