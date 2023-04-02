9103b48388d763c5a4178d500128f0e8ebc60914
Francis Kamara <111047328+devkamara@users.noreply.github.com>
2023-04-02 07:14:33 +0300

    add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-No modules imported
-"""
-
-
 def add_integer(a, b=98):
-    """
-    Returns an integer addition of a and b
-    Float are to be casted to integers
-    Raises:
-    TypeError: if a and b are not integers or floats
-    """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    """function that add 2 numbers"""
+
+    if not a or (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+
+    return(int(a) + int(b))
